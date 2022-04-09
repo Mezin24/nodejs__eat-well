bd0441999e705f696a9978d6d137186fdd0d64aa
Pavel Mezencev <mezencev24@mail.ru>
2022-04-09 16:03:38 +0300

    add routes and change routs in html

---
+++ app.js
@@ -0,0 +1,29 @@
+const fs = require('fs')
+const path = require('path')
+const express = require('express')
+const app = express()
+
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', (req, res) => {
+  const filePath = path.join(__dirname, 'views', 'index.html')
+  res.sendFile(filePath)
+})
+app.get('/about', (req, res) => {
+  const filePath = path.join(__dirname, 'views', 'about.html')
+  res.sendFile(filePath)
+})
+app.get('/confirm', (req, res) => {
+  const filePath = path.join(__dirname, 'views', 'confirm.html')
+  res.sendFile(filePath)
+})
+app.get('/recommend', (req, res) => {
+  const filePath = path.join(__dirname, 'views', 'recommend.html')
+  res.sendFile(filePath)
+})
+app.get('/restaurants', (req, res) => {
+  const filePath = path.join(__dirname, 'views', 'restaurants.html')
+  res.sendFile(filePath)
+})
+
+app.listen(3000)
