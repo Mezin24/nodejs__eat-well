e88f03bf000f1e112b2fdae7c78b1a1df0a8ebb7
Pavel Mezencev <mezencev24@mail.ru>
2022-04-09 16:44:03 +0300

    add form submition

--- app.js
+++ app.js
@@ -10,18 +10,31 @@ app.get('/', (req, res) => {
   const filePath = path.join(__dirname, 'views', 'index.html')
   res.sendFile(filePath)
 })
+
 app.get('/about', (req, res) => {
   const filePath = path.join(__dirname, 'views', 'about.html')
   res.sendFile(filePath)
 })
+
 app.get('/confirm', (req, res) => {
   const filePath = path.join(__dirname, 'views', 'confirm.html')
   res.sendFile(filePath)
 })
+
 app.get('/recommend', (req, res) => {
   const filePath = path.join(__dirname, 'views', 'recommend.html')
   res.sendFile(filePath)
 })
+
+app.post('/recommend', (req, res) => {
+  const filePath = path.join(__dirname, 'data', 'restaurants.json')
+  const newRestaurant = req.body
+  const restaurants = JSON.parse(fs.readFileSync(filePath))
+  restaurants.push(newRestaurant)
+  fs.writeFileSync(filePath, JSON.stringify(restaurants))
+  res.redirect('/confirm')
+})
+
 app.get('/restaurants', (req, res) => {
   const filePath = path.join(__dirname, 'views', 'restaurants.html')
   res.sendFile(filePath)
